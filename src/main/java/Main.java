d181ea57bc56c5c213dfc41ed510779fbc8efc54
Drlazinho <lazbonfim@hotmail.com>
2022-03-25 16:36:25 -0300

    Mentoria e Curso

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHorario(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Kotlin");
+        curso2.setDescricao("descrição curso Kotlin");
+        curso2.setCargaHorario(30);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descriçao mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
